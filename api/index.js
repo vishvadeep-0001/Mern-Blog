c157f1ef6d0f25c1befc3329635b9e9e0f1e2ad1
Vishvadeep <vishvadeep.0001@example.com>
2025-08-27 21:38:15 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
-dotenv.config();
+dotenv.config(); 
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,9 +17,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 let PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
  
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
