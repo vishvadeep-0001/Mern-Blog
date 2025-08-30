ee52c1a93a0b0a160d820ce3b9e965f24008f412
Vishvadeep <vishvadeep.0001@example.com>
2025-08-30 22:50:07 +0530

    complete signin functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,113 @@
-import React from "react";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  return <div>Signin</div>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* Left Side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Yogesh's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is demo project. you can signin with your email and password.
+            or with Gmail Account
+          </p>
+        </div>
+        {/* Right Side */}
+        <div className="ml-5">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="email">Email</Label>
+              <TextInput
+                type="email"
+                placeholder="Your Email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your Password">Password</Label>
+              <TextInput
+                type="password"
+                placeholder="Your Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              className="bg-gradient-to-br from-purple-600 to-pink-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "SignIn"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account ?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              SignUp
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -103,7 +103,7 @@ const SignUp = () => {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account ?</span>
-            <Link to="/" className="text-blue-500">
+            <Link to="/sign-in" className="text-blue-500">
               SignIn
             </Link>
           </div>
