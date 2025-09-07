ce6c764955b243bc7f2abc935f38740254cb2d1f
Vishvadeep <vishvadeep.0001@example.com>
2025-09-07 14:33:58 +0530

    add delete user api route and controller

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -56,3 +56,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: "User has been deleted successfully" });
+  } catch (error) {
+    next(errorHandler(error));
+  }
+};
