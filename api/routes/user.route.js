1aa6cd8aad9f6d818e60115a40887c9caf802118
Vishvadeep <vishvadeep.0001@example.com>
2025-09-22 22:22:45 +0530

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { deleteUser, signout, test, updateUser } from "../controller/user.controller.js";
+import {
+  deleteUser,
+  getUsers,
+  signout,
+  test,
+  updateUser,
+} from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,6 +13,7 @@ const router = express.Router();
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post("/signout", signout)
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
